392ccf181d236fea597ea2d6516e079f70a46cb2
Magnew-98 <markosagnew@gmail.com>
2026-05-30 20:07:13 +0100

    dry run 1 results

---
+++ run_experiment.ps1
@@ -0,0 +1,5 @@
+﻿#Requires -Version 5.1
+$ErrorActionPreference = 'Stop'
+$ProjectDir = Split-Path -Parent $MyInvocation.MyCommand.Path
+Set-Location $ProjectDir
+& (Join-Path $ProjectDir '.venv\Scripts\python.exe') -m src.evaluation.run_experiment @args
